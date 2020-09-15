1bc103fe8b32b5fc82921e7d0dd7b9836d01a04d
mario2018 <boxforapplecny@163.com>
2020-09-15 18:08:26 +0800

    modification of style but only part of

--- ReverseLinkedList/revLinkedList.py
+++ ReverseLinkedList/revLinkedList.py
@@ -4,38 +4,39 @@ class ListNode:
         self.val = val
         self.next = next
 
+
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-    	# solution faster (iterative)
-    	prev, curr = None, head
+        # solution faster (iterative)
+        prev, curr = None, head
 
-    	while curr:
-    		curr.next, prev, curr = prev, curr, curr.next
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
 
-    	return prev
-
-        # ####################################
-    	# solution not faster enough (iterative)
-    	# new_head = None
-
-    	# while head:
-    	# 	temp = head.next
-    	# 	head.next = new_head
-    	# 	new_head = head
-    	# 	head = temp
-
-    	# return new_head
+        return prev
 
         # ####################################
-    	# solution (recursive)
-    	# NEED pay attention to the handling of the first and second node
-    	# if (head == None or head.next == None) : return head
+        # solution not faster enough (iterative)
+        # new_head = None
 
-    	# prev = self.reverseList(head.next)
-    	# head.next.next = head
-    	# head.next = None
+        # while head:
+        # 	temp = head.next
+        # 	head.next = new_head
+        # 	new_head = head
+        # 	head = temp
 
-    	# return prev
+        # return new_head
+
+        # ####################################
+        # solution (recursive)
+        # NEED pay attention to the handling of the first and second node
+        # if (head == None or head.next == None) : return head
+
+        # prev = self.reverseList(head.next)
+        # head.next.next = head
+        # head.next = None
+
+        # return prev
 
 
 class LinkList:
@@ -43,26 +44,28 @@ class LinkList:
         self.head = None
 
     def initList(self, data):
-    	if (len(data) == 0):return None
-    	self.head = ListNode(data[0])
-    	r = self.head
-    	p = self.head
+        if len(data) == 0:
+            return None
+        self.head = ListNode(data[0])
+        r = self.head
+        p = self.head
 
-    	for i in data[1:]:
-        	node = ListNode(i)
-        	p.next = node
-        	p = p.next
-    	return r
+        for i in data[1:]:
+            node = ListNode(i)
+            p.next = node
+            p = p.next
+        return r
 
     def printList(self, head):
         if head:
             node = head
         else:
-        	return
+            return
         while node:
             print(node.val, end=' ')
             node = node.next
         print('\n')
+
 
 def main():
     l = LinkList()
@@ -71,6 +74,7 @@ def main():
     l1 = l.initList(data)
     lRev = s.reverseList(l1)
     l.printList(lRev)
+
 
 if __name__ == '__main__':
     main()
